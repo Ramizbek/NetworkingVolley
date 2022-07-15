7f565fc0277dd590c7aa451d08cfbc8254bc2d7b
Ramizbek <ramizbek.aliyev@gmail.com>
2022-07-15 10:10:02 +0500

    ConnectedOrDisconnectedToInternet

--- app/src/main/java/ramizbek/aliyev/networkingvolley/MainActivity.kt
+++ app/src/main/java/ramizbek/aliyev/networkingvolley/MainActivity.kt
@@ -3,16 +3,21 @@ package ramizbek.aliyev.networkingvolley
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import ramizbek.aliyev.networkingvolley.databinding.ActivityMainBinding
+import ramizbek.aliyev.networkingvolley.utils.MyNetworkHelper
 import ramizbek.aliyev.networkingvolley.utils.isNetworkConnected
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private lateinit var myNetworkHelper: MyNetworkHelper
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        if(isNetworkConnected(this)){
+        myNetworkHelper = MyNetworkHelper(this)
+
+//        if(isNetworkConnected(this)){
+        if(myNetworkHelper.isNetworkConnected()){
             binding.tvInfo.text = "Connected"
         }else{
             binding.tvInfo.text = "Disconnected"
